b78b013de56f89e896a978d54818ffe379426ea6
Pedro Oliveira <pedro.oliveira@pcivil.rj.gov.br>
2024-08-09 18:54:03 -0300

    fix

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -10,10 +10,6 @@ config();
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
     super({
-      clientID:
-        '329851392487-7ruttp0342cdm77qlaeoldsvfh5doldn.apps.googleusercontent.com', // insira seu client ID do Google
-      clientSecret: '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', // insira seu client secret do Google
-      callbackURL: 'http://localhost:8080/auth/google/callback',
       scope: ['email', 'profile'],
     });
   }
